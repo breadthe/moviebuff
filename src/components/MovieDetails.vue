2369e605c78a3ade132bac6e18e07f3af4feede0
breadthe <ferdinand.bismarck@gmail.com>
2018-05-10 17:23:59 -0500

    Movie details modal

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="modal movie-details" :class="{'is-active': detailsIsOpen}">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+
+          <div class="card">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-128x128">
+                    <img :src="movie.Poster" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">{{movie.Title}}</p>
+                  <p class="subtitle is-6">{{movie.Year}}</p>
+                  <p>{{movie.Runtime}}</p>
+                  <p>{{movie.Genre}}</p>
+                  <br>
+                  <p><strong>Released:</strong> {{movie.Released}}</p>
+                  <p><strong>Cast:</strong> {{movie.Actors}}</p>
+                  <br>
+                  <p>{{movie.Plot}}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+    </div>
+    <button class="modal-close is-large" aria-label="close" @click="$emit('closeDetails')"></button>
+  </div>
+</template>
+
+<script>
+// import store from '@/store'
+
+export default {
+  name: 'MovieDetails',
+  props: {
+    'movie': {
+      type: Object,
+      required: true
+    },
+    'details-is-open': {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -5,7 +5,6 @@
           <button class="button is-success" @click="moveToSeenlist(movie.imdbID)">
             <i class="fa fa-eye" aria-hidden="true"></i>&nbsp;Seen
           </button>
-          <!-- <button class="button is-danger" @click="removeFromWishlist(movie.imdbID)"> -->
           <button class="button is-danger" @click="removeFromWishlist(movie.imdbID)">
             <i class="fa fa-times" aria-hidden="true"></i>&nbsp;Remove
           </button>
@@ -17,18 +16,27 @@
         </div>
       </div>
       <div class="movie-poster">
-        <img :src="movie.Poster">
+        <img :src="movie.Poster" @click="detailsIsOpen = true">
       </div>
-      <div class="movie-title">{{movie.Title}}</div>
-      <div>{{movie.Year}}</div>
+      <div @click="detailsIsOpen = true">
+        <div class="movie-title">{{movie.Title}}</div>
+        <div>{{movie.Year}}</div>
+      </div>
+
+      <movie-details :movie="movie" :details-is-open="detailsIsOpen" @closeDetails="detailsIsOpen = false"></movie-details>
+
   </div>
 </template>
 
 <script>
 import store from '@/store'
+import MovieDetails from '@/components/MovieDetails'
 
 export default {
   name: 'MovieItem',
+  components: {
+    'movie-details': MovieDetails
+  },
   props: {
     'movie': {
       type: Object,
@@ -39,8 +47,9 @@ export default {
       required: false
     }
   },
-  data () {
+  data: function () {
     return {
+      detailsIsOpen: false
     }
   },
   methods: {
