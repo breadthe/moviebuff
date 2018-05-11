cad24ef8c11aaef72a8cb8b7b94350aef56f55ef
breadthe <ferdinand.bismarck@gmail.com>
2018-05-11 09:04:49 -0500

    Incredibly basic stats on the Dashboard

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
     <section class="section">
-      <h1 class="title is-1">Dashboard</h1>
+      <h1 class="title is-2">Dashboard</h1>
+      <br>
+      <p class="subtitle is-6" v-if="seenlist.length"><strong>{{seenlist.length}}</strong> movies seen</p>
+      <p class="subtitle is-6" v-else>You haven't seen any movies yet. Go watch some!</p>
+      <p class="subtitle is-6" v-if="wishlist.length"><strong>{{wishlist.length}}</strong> movies on your wishlist</p>
     </section>
   </div>
 </template>
 
 <script>
+import store from '@/store'
+import { mapState } from 'vuex'
+
 export default {
   name: 'Dashboard',
   data () {
     return {
       msg: 'Dashboard'
     }
+  },
+  computed: {
+    ...mapState(['seenlist', 'wishlist'])
   }
 }
 </script>
